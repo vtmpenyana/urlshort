49f3af034edfb7e467fb779463395dd09032dd9e
Vincent-Mpenyana <vincent.mpenyana8@gmail.com>
2020-04-19 16:34:37 +0200

    Updates

---
+++ precache-manifest.e5fb7c0e2476ea795ad37920107c4ead.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea4327c59bdc54680de5aed168b3898",
+    "url": "/urlshort/index.html"
+  },
+  {
+    "revision": "eb8a05c521484e55f386",
+    "url": "/urlshort/static/css/2.3335cff1.chunk.css"
+  },
+  {
+    "revision": "07d93a4b4696fb882f35",
+    "url": "/urlshort/static/css/main.13f0d5cf.chunk.css"
+  },
+  {
+    "revision": "eb8a05c521484e55f386",
+    "url": "/urlshort/static/js/2.d978b7ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/urlshort/static/js/2.d978b7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d93a4b4696fb882f35",
+    "url": "/urlshort/static/js/main.b7281e65.chunk.js"
+  },
+  {
+    "revision": "b60bf17fce120b3f82ce",
+    "url": "/urlshort/static/js/runtime-main.c40c783b.js"
+  },
+  {
+    "revision": "be49254091ef6e3c3c109ab371000612",
+    "url": "/urlshort/static/media/logo.be492540.png"
+  },
+  {
+    "revision": "cec7965b96905047d9508f40afc11d21",
+    "url": "/urlshort/static/media/paperwork.cec7965b.png"
+  }
+]);
